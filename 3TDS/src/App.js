34529bf9e144a65972272e617e6d55ea6034ba90
JonPer3z <perez.joaopedro@gmail.com>
2025-08-26 15:17:00 -0300

    Endereço_SDK

--- 3TDS/src/App.js
+++ 3TDS/src/App.js
@@ -3,17 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "../screens/HomeScreen";
+import StructureLayout from "../screens/StructureLayout";
 
 const Drawer = createDrawerNavigator();
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen 
+          name="Home" 
+          component={HomeScreen}>
+          </Drawer.Screen>
         <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
+          name="Struture"
+          component={StructureLayout}
         ></Drawer.Screen>
       </Drawer.Navigator>
     </NavigationContainer>
